a89a0037208360fb15d76e2b1e01d0dc204d1194
haroon7 <anwarbaigharoon@hotmail.com>
2016-04-21 17:56:44 +0200

    Framework c sharp

---
+++ Spryd.Serveur/Spryd.Serveur/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Spryd.Serveur.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Login
+        [Route("User")]
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
