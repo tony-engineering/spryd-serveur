2104edb7e74082ee7ae717229ede58edd8e57c7b
haroon7 <anwarbaigharoon@hotmail.com>
2016-04-22 16:51:00 +0200

    Ajout des classes du modèles Ajout de la DAL et de la connexion MySQL Ajout des WebService USER

--- Spryd.Serveur/Spryd.Serveur/Global.asax.cs
+++ Spryd.Serveur/Spryd.Serveur/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Spryd.Serveur.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,9 @@ namespace Spryd.Serveur
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        /// <summary>
+        /// Démarrage de l'application
+        /// </summary>
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
---
+++ Spryd.Serveur/Spryd.Serveur/Models/SprydZone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Spryd.Serveur.Models
+{
+    public class SprydZone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Beacon Beacon { get; set; }
+    }
+}
