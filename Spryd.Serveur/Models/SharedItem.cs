3481151602dac9c046528e502bb209e5cd75f5e8
arougeot <rougeot.antoine@gmail.com>
2016-04-26 20:34:51 +0100

    Suppression dossier racine inutile + fichier .gitignore

---
+++ Spryd.Serveur/Models/SharedItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Spryd.Serveur.Models
+{
+    public class SharedItem
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime CreateDate { get; set; }
+        public Session Session { get; set; }
+        public string PathUrl { get; set; }
+    }
+}
