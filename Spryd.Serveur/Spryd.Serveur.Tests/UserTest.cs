bad7075c73db0f90765afb76aa8e63ba57eedcf1
haroon7 <anwarbaigharoon@hotmail.com>
2016-04-26 06:49:30 +0200

    Ajout des classes user oublié

---
+++ Spryd.Serveur/Spryd.Serveur.Tests/UserTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spryd.Serveur.Models;
+using Spryd.Serveur.Controllers;
+using System.Web.Http;
+
+namespace Spryd.Serveur.Tests
+{
+    [TestClass]
+    public class UserTest
+    {
+        private IDal dal;
+        private UserController userController;
+
+        [TestInitialize]
+        public void FillUserList()
+        {
+            dal = new FakeDal();
+            userController = new UserController(dal);            
+        }
+
+        [TestMethod]
+        public void GetExistingUser_Success()
+        {
+            User newUser = new User() { Id = 1, Name = "Haroon", Surname = "ANWARBAIG", Email = "haroon@spryd.io", Password = "azerty" };
+            dal.AddUser(newUser);
+            Assert.AreEqual(userController.GetUser(1), newUser);
+        }
+
+        [TestMethod]
+        public void GetNotExistingUser_Fail()
+        {
+            var response = new HttpResponseException(System.Net.HttpStatusCode.NotFound);
+            Assert.AreEqual(userController.GetUser(1), response);
+            // à continuer
+        }
+    }
+}
